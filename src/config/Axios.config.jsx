5072e95f76040b774f418511210ba3f271945d70
francisco <franciscobarrera968@gmail.com>
2022-04-19 18:23:19 -0300

    Se arreglaron problemas de variables parte 6

--- src/config/Axios.config.jsx
+++ src/config/Axios.config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL:`${process.env.REACT_APP_URL_BACKEND}/api`
+    baseURL:`https://memes-web.herokuapp.com/api`
 })
 
 export default clienteAxios
