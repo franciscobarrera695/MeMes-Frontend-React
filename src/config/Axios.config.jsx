641ccc33300b24d5cc3d97be732bbc292f6fdfb0
francisco <franciscobarrera968@gmail.com>
2022-05-11 16:40:34 -0300

    Se oculto url,tarde pero bueno xd

--- src/config/Axios.config.jsx
+++ src/config/Axios.config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL:`http://localhost:5000/api`
+    baseURL:process.env.REACT_APP_URL_BACKEND
 })
 
 export default clienteAxios
